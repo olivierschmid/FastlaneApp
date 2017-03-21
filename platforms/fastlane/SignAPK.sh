9bc1e520444decd00dc63965572e08cf834c5b2a
olivierschmid <olivier.schmid@mobi.ch>
2017-03-21 13:20:26 +0100

    removed

--- platforms/fastlane/SignAPK.sh
+++
@@ -1,5 +0,0 @@
-
-
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/AppDev/mobiliarCustomerApp.keystore ../android/build/outputs/apk/android-release-unsigned.apk mobiliarCustomerApp
-rm ../android/build/outputs/apk/fastlaneApp_signed.apk
-~/Library/Android/sdk/build-tools/25.0.2/zipalign -v 4 ../android/build/outputs/apk/android-release-unsigned.apk ../android/build/outputs/apk/fastlaneApp_signed.apk
--- platforms/fastlane/sign_and_zip.sh
+++
@@ -1,3 +0,0 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/AppDev/mobiliarCustomerApp.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk mobiliarCustomerApp
-
-~/Library/Android/sdk/build-tools/24.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk mobiliarCustomerApp.apk
