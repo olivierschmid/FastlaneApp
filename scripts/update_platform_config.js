db8b71401675e907faea96d0f2bcf9b6e003faf2
olivierschmid <olivier.schmid@mobi.ch>
2017-03-22 13:53:27 +0100

    call plistbuddy

--- scripts/update_platform_config.js
+++ scripts/update_platform_config.js
@@ -2,4 +2,22 @@
 
 // This hook updates platform configuration files
 
- console.log("Start update platform configuration files");
+module.exports = function (ctx) {
+
+
+//console.log('**************** hook ctx: ' + JSON.stringify(ctx));
+if (!ctx) {
+console.log('no context');
+return;
+}
+console.log("Start update platform configuration files");
+
+
+var exec = require('child_process').exec;
+var cmd = '/usr/libexec/PlistBuddy -c "Add :ITSAppUsesNonExemptEncryption bool false" FastlaneApp-Info.plist';
+
+exec(cmd, function(error, stdout, stderr) {
+  // command output is in stdout
+});
+
+  
