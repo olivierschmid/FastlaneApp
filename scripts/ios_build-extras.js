6307483ad2bafb056c0f5d63d0728d2f2d80f1bb
olivierschmid <olivier.schmid@mobi.ch>
2017-03-23 16:01:05 +0100

    read appName from config.xml

--- scripts/ios_build-extras.js
+++ scripts/ios_build-extras.js
@@ -11,13 +11,36 @@ module.exports = function (ctx) {
 
 	console.log('ios_build-extras ...');
 
+	var appName = "";
 	var projectRoot = ctx.opts.projectRoot;
-	var applicationName = projectRoot.substring(projectRoot.lastIndexOf("/")+1);
-	var plistFile = projectRoot + "/platforms/ios/"+applicationName+"/"+applicationName+"-Info.plist";
+	var filePath = projectRoot + "/config.xml";
+
+
+	// Read AppName from config.xml
+	var fs = require('fs');
+	var xml2js = require('xml2js');
+	var json = "";
+	try {
+	    var fileData = fs.readFileSync(filePath, 'ascii');
+	    var parser = new xml2js.Parser();
+	    parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+	        json = result;
+	    });
+	    console.log("File '" + filePath + "' was successfully read.");
+
+		appName = json.widget.name;
+		console.log("Application Name:", appName[0]);
+	 }
+	 catch (e) {
+	    console.log(e)
+	 }
+
+	// Update Entries in Info.plist with PlistBuddy Tool
+	var plistFile = projectRoot + "/platforms/ios/" + appName + "/" + appName + "-Info.plist";
 	var exec = require('child_process').exec;
 	var cmd = '/usr/libexec/PlistBuddy -c "Add :ITSAppUsesNonExemptEncryption bool false" ' + plistFile;
 
-	console.log("Updating File: "+plistFile);
+	console.log("Updating File: " + plistFile);
 
 	try {
 		exec(cmd);
